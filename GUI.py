e786b1589f785af3a37da15e2a21b37634beb702
ApfelinoQL <107317602+ApfelinoQL@users.noreply.github.com>
2023-09-15 00:20:50 +0200

    Updated README, also completely forgot that you could not only write integers inside Entry :skull:

--- GUI.py
+++ GUI.py
@@ -6,31 +6,37 @@ import time
 def Setbuttonclick():
     SecondsTime = ST.get()
     MinutesTime = MT.get()
-    if SecondsTime == "" or MinutesTime == "":
+    try:
+        int(SecondsTime)
+        int(MinutesTime)
+    except ValueError:
         pass
-    elif int(SecondsTime) <= -1 or int(MinutesTime) <= -1:
-        pass
-    elif int(SecondsTime) == 0 and int(MinutesTime) == 0:
-        pass
-
     else:
-        if int(SecondsTime) < 10:
-            TimeDis.config(text="Timer set with " + str(MinutesTime) + ":0" + str(SecondsTime))
+        if SecondsTime == "" or MinutesTime == "":
+            pass
+        elif int(SecondsTime) <= -1 or int(MinutesTime) <= -1:
+            pass
+        elif int(SecondsTime) == 0 and int(MinutesTime) == 0:
+            pass
+
         else:
-            TimeDis.config(text="Timer set with " + str(MinutesTime) + ":" + str(SecondsTime))
-        while True:
-            print(str(MinutesTime) + " " + str(SecondsTime))
-            if int(MinutesTime) == 0:
-                print("SSScript starting")
-                root.after(500)
-                ST.delete(0, tk.END)
-                Secondsscript.Countdown(SecondsTime)
+            if int(SecondsTime) < 10:
+                TimeDis.config(text="Timer set with " + str(MinutesTime) + ":0" + str(SecondsTime))
             else:
-                print("MSScript starting")
-                root.after(500)
-                ST.delete(0, tk.END)
-                MT.delete(0, tk.END)
-                Minutesscript.Countdown(MinutesTime, SecondsTime)
+                TimeDis.config(text="Timer set with " + str(MinutesTime) + ":" + str(SecondsTime))
+            while True:
+                print(str(MinutesTime) + " " + str(SecondsTime))
+                if int(MinutesTime) == 0:
+                    print("SSScript starting")
+                    root.after(500)
+                    ST.delete(0, tk.END)
+                    Secondsscript.Countdown(SecondsTime)
+                else:
+                    print("MSScript starting")
+                    root.after(500)
+                    ST.delete(0, tk.END)
+                    MT.delete(0, tk.END)
+                    Minutesscript.Countdown(MinutesTime, SecondsTime)
     print("Invalid values")
     TimeDis.config(text="Invalid values! Try again")
 
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SimpleTimer by ApfelinoQL
 I just wanted to do something while outside of school, and since im quite new to coding, I decided to code a simple Timer with a tkinter
-gui using a lot of help from chatgpt. 
+gui using a lot of help from ChatGPT. 
 
-13.09.2023 - Timer function works, GUI needs some more work, No sound
+- 13.09.2023 - Timer function works, GUI needs some more work, No sound 
+- 14.09.2023 - Fixed GUI compatibility with Timer script, removed TimerScript.py after intergrating most of it in GUI.py. I'm aware that Secondsscript.py can be easily replaced by Minutesscript.py, but don't change a working program as they say :)
